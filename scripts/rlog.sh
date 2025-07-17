e0dcaf8bb3cfa46daadb8ae72af9be623d5fdb34
Shudipta Sharma <alittleprogramming@gmail.com>
2025-07-17 13:07:17 +0600

    Update rlog.sh

--- scripts/rlog.sh
+++ scripts/rlog.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 SHOW_LOG=0
+CHART_PATH=""
+TAG_BUILD=0
 
 function show_help() {
     echo "rlog.sh - generate a release log file from based on the commits on top of the last tag"
@@ -10,6 +12,7 @@ function show_help() {
     echo "options:"
     echo "-h, --help          show brief help"
     echo "-v                  show the contents of the log file"
+    echo "--chart-path, -c    path to the helm chart"
 }
 
 while test $# -gt 0; do
@@ -19,7 +22,19 @@ while test $# -gt 0; do
       exit 0
       ;;
     -v)
-      export SHOW_LOG=1
+      SHOW_LOG=1
+      shift
+      ;;
+    -c | --chart-path)
+      CHART_PATH="$2"
+      shift 2
+      ;;
+    --chart-path=*)
+      CHART_PATH="${1#*=}"
+      shift
+      ;;
+    --tag-build)
+      TAG_BUILD=1
       shift
       ;;
     *)
@@ -42,7 +57,7 @@ log="# Version ${new_tag} (${today_date})
 
 ## Features / Fixes
 
-$(git log --pretty=format:"* %B [%h](${remote}/commit/%H)" ${old_tag}..)
+$(git log --no-merges --pretty=format:"* %B [%h](${remote}/commit/%H)" ${old_tag}.. | grep -v '^Signed-off-by:' | sed '/^\s*$/d')
 
 ## Diff
 
@@ -58,9 +73,43 @@ if [ ${SHOW_LOG} = 1 ]; then
     echo "-----------------------------"; echo
 fi
 
-BOLD_START="\e[1;4m"; BOLD_END="\e[0m"
+BOLD_START="\033[1;4m"; BOLD_END="\033[0m"
 echo -e "${BOLD_START}NOTE: The newly created release file may need to be edited/formatted depending on the need(s) before committing.${BOLD_END}"; echo
 
 printf "${log}" > ${release_filepath}
+
+# Change the app container version that is going to live
+if [[ -n "${CHART_PATH}" ]]; then
+    os_type="$(uname)"
+    echo "==> updating the chart file.."
+    if [[ "$os_type" == "Darwin" ]]; then
+        sed -i '' "s/^appVersion: .*/appVersion: ${new_tag}/" "${CHART_PATH}/Chart.yaml"
+    elif [[ "$os_type" == "Linux" ]]; then
+        sed -i "s/^appVersion: .*/appVersion: ${new_tag}/" "${CHART_PATH}/Chart.yaml"
+    else
+        echo "Unknown OS: $os_type"
+    fi
+
+    echo
+fi
+
+if [ ${TAG_BUILD} = 1 ]; then
+    echo "==> commiting.."
+    git add ${release_filepath} ${CHART_PATH}
+    git commit -m "$(echo "added release logs for tag ${new_tag}")"
+    echo
+
+    echo "==> input a tag message"
+    read tag_msg;
+    echo
+
+    echo "==> creating tag ${new_tag}"
+    git tag ${new_tag} -a -m "$(echo "${new_tag}")"
+    echo
+
+    echo "==> push the release commit with tag ${new_tag}"
+    git push origin HEAD --tags
+    echo
+fi
 
 echo "Enjoy !!!"
